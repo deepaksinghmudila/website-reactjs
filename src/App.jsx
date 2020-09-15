b40afa332e994c33df71cab673056ee24990bca4
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2020-09-15 21:48:19 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Home from "./Home";
 import Contact from "./Contact";
 import Service from "./Service";
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Footer = () => {
    return (
-     <>
-       <footer className="w-100 bg-light text-center">
-         <p> © 2020 DStudio. All Rights Reserved | Terms and Conditions</p>
-       </footer>
+     <>     
+         <footer className=" bg-light text-center bottom-0">
+             <p> © 2020 DStudio. All Rights Reserved | Terms and Conditions</p>
+         </footer>
+        
      </>
    );
 };
